a2d025b0b6e4cf3617e7c3791cbc99512b8fb75c
Nathan6175 <75102322+Nathan6175@users.noreply.github.com>
2022-09-01 10:06:39 -0300

    Update botao_menu.js

--- botao_menu.js
+++ botao_menu.js
@@ -1,31 +1,37 @@
-import {WiWindy} from "react-icons/wi";
+import { WiWindy } from "react-icons/wi";
 import { View, Text } from "react-native-web";
 import { StyleSheet } from "react-native";
 
 export function Botao_menu() {
-    return(
+    return (
         <View style={styles.container}>
-            <View style={styles.container3}><WiWindy size={32}/></View>
-            <View style={styles.container2}><Text>...</Text></View>
+            <View style={styles.container3}><WiWindy size={32} /></View>
+            <View style={styles.container2}>
+                <Text style={styles.container4} >...</Text>
         </View>
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      flexDirection:"row",
-      
-    },
-    container2:{
         flex: 1,
-        justifyContent:"flex-end",
-        flexDirection:"row",
-        paddingRight:10
+        flexDirection: "row",
+
     },
-    container3:{
+    container2: {
         flex: 1,
-        justifyContent:"flex-start",
-        flexDirection:"row",
-        paddingLeft:10
-    }})
+        justifyContent: "flex-end",
+        flexDirection: "row",
+        paddingRight: "7.5%",
+    },
+    container3: {
+        flex: 1,
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        paddingLeft: "7.5%"
+    },
+    container4: {
+        fontSize: 20
+    }
+})
